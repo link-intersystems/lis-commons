acfb16014ece0e7074c858f98e496b4540c8776c
René Link <rene.link@link-intersystems.com>
2022-04-19 08:59:44 +0200

    Refactoring tools messed up class.

--- lis-commons-beans/src/main/java/com/link_intersystems/beans/PropertyDescList.java
+++ lis-commons-beans/src/main/java/com/link_intersystems/beans/PropertyDescList.java
@@ -20,25 +20,25 @@ public class PropertyDescList extends AbstractList<PropertyDesc> {
     }
 
     public List<String> getAllPropertyNames() {
-        return stream().map(com.link_intersystems.beans.PropertyDesc::getName).collect(Collectors.toList());
+        return stream().map(PropertyDesc::getName).collect(Collectors.toList());
     }
 
     /**
      * @return all properties that are not indexed properties.
      */
     public List<String> getPropertyNames() {
-        return stream().filter(pd -> !(pd instanceof IndexedPropertyDesc)).map(com.link_intersystems.beans.PropertyDesc::getName).collect(Collectors.toList());
+        return stream().filter(pd -> !(pd instanceof IndexedPropertyDesc)).map(PropertyDesc::getName).collect(Collectors.toList());
     }
 
     public List<String> getIndexedPropertyNames() {
-        return stream().filter(IndexedPropertyDesc.class::isInstance).map(com.link_intersystems.beans.PropertyDesc::getName).collect(Collectors.toList());
+        return stream().filter(IndexedPropertyDesc.class::isInstance).map(PropertyDesc::getName).collect(Collectors.toList());
     }
 
-    public com.link_intersystems.beans.PropertyDesc getByName(String propertyName) {
+    public PropertyDesc getByName(String propertyName) {
         return getByName(propertyName, Objects::equals);
     }
 
-    public com.link_intersystems.beans.PropertyDesc getByName(String propertyName, Equality<String> nameEquality) {
+    public PropertyDesc getByName(String propertyName, Equality<String> nameEquality) {
         return stream().filter(pd -> nameEquality.isEqual(propertyName, pd.getName())).findFirst().orElse(null);
     }
 
