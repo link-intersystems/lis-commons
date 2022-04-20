110f187b658e5cc064a58b886bd5aff979f505bd
René Link <rene.link@link-intersystems.com>
2022-04-20 08:28:31 +0200

    Added tests for predicates.

--- lis-commons-util/src/main/java/com/link_intersystems/util/OrPredicate.java
+++ lis-commons-util/src/main/java/com/link_intersystems/util/OrPredicate.java
@@ -8,14 +8,14 @@ import static java.util.Arrays.asList;
 /**
  * @author René Link {@literal <rene.link@link-intersystems.com>}
  */
-public class OrAbstractPredicate<T> extends AbstractPredicateCollection<T> {
+public class OrPredicate<T> extends AbstractPredicateCollection<T> {
 
     @SafeVarargs
-    public OrAbstractPredicate(Predicate<T>... predicates) {
+    public OrPredicate(Predicate<T>... predicates) {
         this(asList(predicates));
     }
 
-    public OrAbstractPredicate(Collection<Predicate<T>> predicates) {
+    public OrPredicate(Collection<Predicate<T>> predicates) {
         super(true, predicates);
     }
 
