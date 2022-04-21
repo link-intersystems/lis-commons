fb6a2ecba76353a8ca3f012d76e1a18e6f11b764
René Link <rene.link@link-intersystems.com>
2022-04-21 10:40:27 +0200

    Added javadoc.

--- lis-commons-util/src/main/java/com/link_intersystems/util/EqualPredicate.java
+++ lis-commons-util/src/main/java/com/link_intersystems/util/EqualPredicate.java
@@ -1,13 +1,18 @@
 package com.link_intersystems.util;
 
-import java.io.Serializable;
 import java.util.Objects;
 import java.util.function.Predicate;
 
 /**
+ * A {@link Predicate} that returns true if an object it is tested against is
+ * equal to the object it was constructed with according to the equal definition
+ * of {@link Objects#equals(Object, Object)}. This is the main difference to using
+ * a method reference like: someObject::equals, because {@link Objects#equals(Object, Object)}
+ * is <code>null</code> safe and contains true if both are <code>null</code>.
+ *
  * @author René Link {@literal <rene.link@link-intersystems.com>}
  */
-public class EqualPredicate<T extends  Serializable> implements Predicate<T>, Serializable {
+public class EqualPredicate<T> implements Predicate<T> {
 
     private T equalTo;
 
