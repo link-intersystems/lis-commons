db68920cda5c44b28b8de913cfe844ab9d080b09
René Link <rene.link@link-intersystems.com>
2023-05-24 06:55:55 +0200

    Refactored ActionTrigger.

--- lis-commons-swing/src/main/java/com/link_intersystems/swing/action/ActionTrigger.java
+++ lis-commons-swing/src/main/java/com/link_intersystems/swing/action/ActionTrigger.java
@@ -8,25 +8,73 @@ import static java.util.Objects.*;
 
 public class ActionTrigger {
 
+    public static final String DEFAULT_COMMAND = "";
+
     public static void performAction(Object actionEventSource, ActionListener actionListener) {
         performAction(actionEventSource, actionListener, "");
     }
 
     public static void performAction(Object actionEventSource, ActionListener actionListener, String command) {
-        actionListener.actionPerformed(new ActionEvent(actionEventSource, ACTION_PERFORMED, command));
+        performAction(actionEventSource, actionListener, command, ACTION_PERFORMED);
+    }
+
+    public static void performAction(Object actionEventSource, ActionListener actionListener, String command, int actionId) {
+        actionListener.actionPerformed(new ActionEvent(actionEventSource, actionId, command));
     }
 
     private Object actionEventSource;
+    private int actionId;
+    private String command;
 
+    /**
+     * Constructs an {@link ActionTrigger} with the {@link #DEFAULT_COMMAND} and the #ACTION_PERFORMED actionId.
+     *
+     * @param actionEventSource
+     */
     public ActionTrigger(Object actionEventSource) {
+        this(actionEventSource, DEFAULT_COMMAND);
+    }
+
+    /**
+     * Constructs an {@link ActionTrigger} with the given command and the #ACTION_PERFORMED actionId.
+     *
+     * @param actionEventSource
+     * @param command
+     */
+    public ActionTrigger(Object actionEventSource, String command) {
+        this(actionEventSource, command, ACTION_PERFORMED);
+    }
+
+    /**
+     * Constructs an {@link ActionTrigger} with the given command and the given actionId.
+     *
+     * @param actionEventSource
+     * @param command
+     * @param actionId
+     */
+    public ActionTrigger(Object actionEventSource, String command, int actionId) {
         this.actionEventSource = requireNonNull(actionEventSource);
+        this.command = requireNonNull(command);
+        this.actionId = actionId;
     }
 
+    /**
+     * Invokes the given actionListener with an {@link ActionEvent} that has this
+     * {@link ActionTrigger}'s actionEventSource as it's {@link ActionEvent#getSource()},
+     * this {@link ActionTrigger}'s command as it's {@link ActionEvent#getActionCommand()}
+     * and this {@link ActionTrigger}'s actionId as it's {@link ActionEvent#getID()} ()}
+     * The {@link ActionEvent#getID()}.
+     * <p>
+     * You can use one of the other methods to perform an action with another command or actionId then this action.
+     *
+     * @param actionListener
+     */
     public void performAction(ActionListener actionListener) {
-        performAction(actionListener, "");
+        performAction(actionEventSource, actionListener, command, actionId);
     }
 
-    public void performAction(ActionListener actionListener, String command) {
-        performAction(actionEventSource, actionListener, command);
+    public Runnable asRunnable(ActionListener actionListener) {
+        return () -> performAction(actionListener);
     }
+
 }
--- lis-commons-swing/src/test/java/com/link_intersystems/swing/action/ActionTriggerTest.java
+++ lis-commons-swing/src/test/java/com/link_intersystems/swing/action/ActionTriggerTest.java
@@ -1,6 +1,5 @@
 package com.link_intersystems.swing.action;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,7 +15,7 @@ class ActionTriggerTest implements ActionListener {
     private ActionTrigger actionTrigger;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         actionEvent = null;
         actionTrigger = new ActionTrigger(this);
     }
@@ -32,11 +31,24 @@ class ActionTriggerTest implements ActionListener {
 
     @Test
     void performActionWithCommand() {
-        actionTrigger.performAction(this, "performActionWithCommand");
+        actionTrigger = new ActionTrigger(this, "performActionWithCommand");
+
+        actionTrigger.performAction(this);
 
         assertEquals(this, actionEvent.getSource());
         assertEquals(ACTION_PERFORMED, actionEvent.getID());
         assertEquals("performActionWithCommand", actionEvent.getActionCommand());
+    }
+
+    @Test
+    void asRunnable() {
+        actionTrigger = new ActionTrigger(this);
+        Runnable runnable = actionTrigger.asRunnable(this);
+
+        runnable.run();
+
+        assertEquals(this, actionEvent.getSource());
+        assertEquals(ACTION_PERFORMED, actionEvent.getID());
     }
 
     @Test
@@ -50,7 +62,7 @@ class ActionTriggerTest implements ActionListener {
 
     @Test
     void staticPerformActionWithCommand() {
-        ActionTrigger.performAction(this,this, "performActionWithCommand");
+        ActionTrigger.performAction(this, this, "performActionWithCommand");
 
         assertEquals(this, actionEvent.getSource());
         assertEquals(ACTION_PERFORMED, actionEvent.getID());
